2d488ec41b0176685a8525a803be3c6af8e3a52e
huunghia-pgts <nghia.nh@pgt-solutions.io>
2025-02-10 23:51:03 +0700

    init commit for backend

---
+++ MUJI-backend/MUJI-backend/src/main/java/com/example/MUJI_backend/MujiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MUJI_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MujiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MujiBackendApplication.class, args);
+	}
+
+}
---
+++ MUJI-backend/MUJI-backend/src/test/java/com/example/MUJI_backend/MujiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MUJI_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MujiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
